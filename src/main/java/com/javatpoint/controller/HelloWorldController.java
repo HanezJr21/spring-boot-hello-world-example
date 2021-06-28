cd19cafd2a9274ab5174ad1100c0e0aae08f999d
admin <admin@izeno.com>
2021-06-28 14:08:51 +0700

    first commit

---
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.javatpoint.controller;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController{
+	private static final Logger log = LogManager.getLogger(HelloWorldController.class);
+
+	@RequestMapping("/")
+	public String hello() {
+		log.info("ACCESS PAGE SUCCESS");
+		return "Hello javaTpoint";
+	}
+}
